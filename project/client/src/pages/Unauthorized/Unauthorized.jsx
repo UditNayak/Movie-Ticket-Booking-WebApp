403c88378e4bcab082acb98885b3b76ea0a008a3
UditNayak <uditnayak027@gmail.com>
2024-07-17 13:48:35 +0530

    Adding styles to Unauthorized Page

--- project/client/src/pages/Unauthorized/Unauthorized.jsx
+++ project/client/src/pages/Unauthorized/Unauthorized.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Typography, Layout } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title, Paragraph } = Typography;
+const { Content } = Layout;
 
 function Unauthorized() {
   return (
-    <div className="unauthorized-container">
-      <h1>Access Denied</h1>
-      <p>You do not have permission to view this page.</p>
-      <Link to="/" className="unauthorized-link">Go to Home Page</Link>
-    </div>
+    <Layout
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Content style={{ textAlign: "center", padding: "20px" }}>
+        <Title level={1} style={{ fontSize: "36px" }}>
+          Access Denied
+        </Title>{" "}
+        <Paragraph style={{ fontSize: "20px", marginBottom: "20px" }}>
+          You do not have permission to view this page.
+        </Paragraph>{" "}
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button type="primary" size="large">
+            Return to Home Page
+          </Button>{" "}
+        </Link>
+      </Content>
+    </Layout>
   );
 }
 
